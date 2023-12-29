bd142f0f9bee29f37a73b0799a2c498266a34db9
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-12-29 10:39:48 -0500

    add

--- src/Component/ToDoForm.js
+++ src/Component/ToDoForm.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useState } from 'react'
 
 const ToDoForm = () => {
+const [newToDo,setNewToDo] = useState('')
+const [deadLine,setDeadLine] = useState('')
+
+
   return (
     <div>
       <input
@@ -15,8 +20,11 @@ const ToDoForm = () => {
       onChange={(e)=>setDeadLine(e.target.value)}
       placeholder='Set Deadline'
       />
-      <button>
-        {}
+      <button onClick={handleAddOrUpdate}>
+        {/* this button have 2 function update to do and add to do  by use ternary operator*/}
+        {
+          edited2Do ? 'Update todo' : 'Add todo'
+        }
       </button>
     </div>
   )
--- src/Component/TodoList.js
+++ src/Component/TodoList.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './ToDoItem'
+import ToDoForm from './ToDoForm'
 
 const TodoList = () => {
     const [toDos,setToDos] = useState([])// this line store all list of todo items --settodo use for put everything inside toDos
@@ -12,7 +13,23 @@ const TodoList = () => {
       setToDos(sortedToDos)
 
     }
+
+    const editeToDoFunc =(toDoValue)=>{
+      setEditedToDo(toDoValue)
+    }
+
+    const addToDoFunc =(newToDoValue)=>{
+//this func take new to do and add to toDos state
+setToDos((prevToDos)=>[...prevToDos,newToDoValue])
+    }
+    const updateToDoFunc =(updatedToDoValue)=>{
+      // 2 thing either store updatedToDovalue or singleToDo
+      const updatedToDos = toDos.map((singleToDo)=>singleToDo.id === updatedToDoValue.id ? updatedToDoValue : singleToDo)
+      setToDos(updatedToDos)
+      setEditedToDo(null)
+    }
     
+
   return (
     <div>
 <h1>Manangement Lists</h1>
@@ -23,9 +40,21 @@ const TodoList = () => {
 <ToDoItem 
 key = {toDo.id}
 Individual2do = {toDo}
+onEdit = {()=>editedToDo(toDos)}
+
+// cotinue next week delete and toggle function 
+
+
+onDelete = {()=>DeleteToDo(toDo.id)}
+onToggleCompleted = {()=>toggleCompleted(toDo.id)}
 />
 ))}
 </ul>
+<ToDoForm 
+editeToDo = {editeToDoFunc}
+onAdd = {addToDoFunc}
+onUpdate = {updateToDoFunc}
+/>
     </div>
   )
 }
